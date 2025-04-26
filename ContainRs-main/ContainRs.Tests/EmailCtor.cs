1b3862d917000d45fadb0f07ead35fa74d09f508
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-26 12:06:24 -0300

    feat: Aplicando um fator importante para Arquitetura Limpa, os testes com padrão AAA.

---
+++ ContainRs-main/ContainRs.Tests/EmailCtor.cs
@@ -0,0 +1,29 @@
+using ContainRs.WebApp.Models;
+
+namespace ContainRs.Tests
+{
+    public class EmailCtor
+    {
+        [Fact]
+        public void Deve_Lancar_ArgumentException_Quando_Valor_Invalido()
+        {
+            //Arrange
+            string emailInvalido = "www.testeinvlaido";
+
+            //Act & //Assert
+            Assert.Throws<ArgumentException>(() => new Email(emailInvalido));
+            
+        }
+
+        [Fact]
+        public void Deve_Validar_Email_Valido_Quando_Valor_Valido()
+        {
+            //Arrange
+            string emailValido = "gustavieiradev@gmail.com";
+            //Act
+            Email email = new Email(emailValido);
+            //Assert
+            Assert.Equal(email.Value, emailValido);
+        }
+    }
+}
