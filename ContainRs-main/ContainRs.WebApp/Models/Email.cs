309c7a1ee538c17d567392ac3709b2ff0c946110
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-26 11:29:03 -0300

    feat: Identificando conceitos essenciais do negócio e aplicando um dos conceitos da arquitetura limpa (camada de domínio).

---
+++ ContainRs-main/ContainRs.WebApp/Models/Email.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ContainRs.WebApp.Models
+{
+    public class Email
+    {
+
+        private static readonly Regex EmailRegex = new Regex(
+            @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        public Email(string value)
+        {
+            if (!EmailRegex.IsMatch(value))
+            {
+                throw new ArgumentException("E-mail invalido.");
+            }
+
+            // lógica de validação
+            Value = value;
+        }
+
+        public string Value { get; }
+    }
+}
